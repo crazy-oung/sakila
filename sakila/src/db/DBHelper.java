c29dcc416037f28deda172faff4bb004ce1eb149
crazy-oung <d_yng@naver.com>
2019-10-04 17:18:31 +0900

    마리아 디비 연동 클래스 작성

---
+++ sakila/src/db/DBHelper.java
@@ -0,0 +1,44 @@
+package db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBHelper {	
+	// db를 연결해주는 메소드
+	   public static Connection getConncetion() throws Exception{
+	       Class.forName("org.mariadb.jdbc.Driver");
+	       Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/sakila","root","java1234");
+	       return conn;
+	   }
+	   
+	   // db를 종료하는 메소드
+	   public static void close(ResultSet rs, Statement stmt, Connection conn) {
+	      if(rs != null) {
+	         try {
+	            rs.close();
+	         } catch (SQLException e) {
+	            e.printStackTrace();
+	         }
+	      }
+	      
+	      if(stmt != null) {
+	         try {
+	            stmt.close();
+	         } catch (SQLException e) {
+	            e.printStackTrace();
+	         }
+	      }
+	      
+	      if(conn != null) {
+	         try {
+	            conn.close();
+	         } catch (SQLException e) {
+	            e.printStackTrace();
+	         }
+	      }
+	   }
+	
+
+}
