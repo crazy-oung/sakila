61121c346e74df5c9440c4a4e718fc601d2bce7c
crazy-oung <creativecloud@kakao.com>
2019-12-24 18:21:04 +0900

    bulma 적용중, ment&footer 자바스크립트 코드로 분리
    - 주소관리에서 도시 리스트 출력 오류 수정 필요

---
+++ sakila/WebContent/js/sakilaCRM.js
@@ -0,0 +1,58 @@
+$('#menu').append(
+		"<aside class='menu'>"+
+		"<h1 class='title is-4'><span class='tag is-warning title is-6'><b>Sakila</b></span> CRM 👾</h1>"+
+		"<p class='menu-label'>Home</p>"+
+		"<ul class='menu-list'><li><a href='/sakila/sakilaIndex.html'>🚧 대시보드</a></li></ul>"+
+		"<p class='menu-label'>Address</p>"+
+		"<ul class='menu-list'>"+
+		"<li><a href='/sakila/address/country.html'>🌍 국가 관리</a></li>"+
+		"<li><a href='/sakila/address/city.html'>🌇 도시 관리</a></li>"+
+		"<li><a href='/sakila/address/address.html'>🏠 주소 관리</a></li>"+
+		"</ul>"+
+		"<p class='menu-label'>Customer</p>"+
+		"<ul class='menu-list'>"+
+		"<li><a href='/sakila/customer/customer.html'>👥 고객 관리</a></li>"+
+		"<li><a href='/sakila/customer/payment.html'>💳 지불 내역</a></li>"+
+		"</ul>"+
+		"<p class='menu-label'Business</p>"+
+		"<ul class='menu-list'>"+
+		"<li><a href='/sakila/business/rental.html'>📇 대여 내역</a></li>"+
+		"<li><a href='/sakila/business/staff.html'>👩‍💼 직원 관리</a></li>"+
+		"<li><a href='/sakila/business/store.html'>🏤 지점 관리</a></li>"+
+		"</ul>"+
+		"<p class='menu-label'>Inventory</p>"+
+		"<ul class='menu-list'>"+
+		"<li><a href='/sakila/inventory/inventory.html'>📉  재고 관리</a></li>"+
+		"<li><a href='/sakila/inventory/inventory.html'>🎥  보유 목록</a></li>"+
+		"</ul>"+
+		"<p class='menu-label'>Setting</p>"+
+		"<ul class='menu-list'>"+
+		"<li><a href='/sakila/setting/language.html'>🌐 언어 관리</a></li>"+
+		"</ul>"+
+		"</aside>");
+
+$("#foot").append(
+		'<footer class="footer">'+
+		'<div class="content has-text-centered">'+
+		'<p>'+
+		'<strong><span class="tag is-warning"><b>Sakila</b></span> CRM 👾</strong> by <a href="https://github.com/crazy-oung">Dayoung Han</a>.<br>'+
+		' Made studying <a href="https://bulma.io/">BULMA</a>. Sample DataBase from <a href="https://dev.mysql.com/doc/sakila/en/">here</a>.'+
+		'</p>'+
+		'</div>'+
+		'</footer>');
+
+	  
+	  
+	
+			  
+			    
+			      
+			     
+			   
+			  
+			
+	
+	
+	  
+	
+
